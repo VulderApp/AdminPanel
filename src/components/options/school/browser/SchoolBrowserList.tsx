58324b7338d695a2d520394dcb1ed9692c3a8414
MRmlik12 <mrmlik12sup@gmail.com>
2022-02-19 20:00:07 +0100

    Add search bar in school browser

--- src/components/options/school/browser/SchoolBrowserList.tsx
+++ src/components/options/school/browser/SchoolBrowserList.tsx
@@ -1,71 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useRecoilValue } from "recoil";
-import { jwtToken } from "../../../../states";
-import { getSchools } from "../../../../api";
-import {
-  CircularProgress,
-  Container,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  Pagination,
-} from "@mui/material";
-import { Schools } from "../../../../api/models/schools/schools";
+import React from "react";
+import { SchoolItem } from "../../../../api/models/schools/schoolItem";
 import { NavigateFunction } from "react-router-dom";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 interface SchoolBrowserListProps {
+  schools: SchoolItem[];
   navigate: NavigateFunction;
 }
 
-const SchoolBrowserList: React.FC<SchoolBrowserListProps> = ({ navigate }) => {
-  const [page, setPage] = useState(1);
-  const [items, setItems] = useState<Schools | null>(null);
-  const token = useRecoilValue(jwtToken);
-
-  const handleBrowserList = async () => {
-    const response = await getSchools(token!, page);
-    if (response.status !== 200) return;
-
-    setItems(response.data);
-  };
-
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) =>
-    setPage(value);
-
-  useEffect(() => {
-    (async () => await handleBrowserList())();
-  }, []);
-
+const SchoolBrowserList: React.FC<SchoolBrowserListProps> = ({
+  schools,
+  navigate,
+}) => {
   return (
-    <Container>
-      {items ? (
-        <List>
-          {items!.schools.map((item) => {
-            return (
-              <ListItem key={item.id}>
-                <ListItemButton
-                  onClick={() => navigate(`/options/school/editor/${item.id}`)}
-                >
-                  <ListItemText primary={item.name} />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-        </List>
-      ) : (
-        <CircularProgress />
-      )}
-      <Container
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Pagination count={items?.pages} onChange={handlePageChange} />
-      </Container>
-    </Container>
+    <List>
+      {schools.map((item) => {
+        return (
+          <ListItem key={item.id}>
+            <ListItemButton
+              onClick={() => navigate(`/options/school/editor/${item.id}`)}
+            >
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
   );
 };
 
