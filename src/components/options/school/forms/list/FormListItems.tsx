277067d9649914efe28115e05dadbffcde0051c2
MRmlik12 <mrmlik12sup@gmail.com>
2022-04-16 19:39:25 +0200

    Add Question mark icon when school form has None status

--- src/components/options/school/forms/list/FormListItems.tsx
+++ src/components/options/school/forms/list/FormListItems.tsx
@@ -8,8 +8,10 @@ import {
 } from "@mui/material";
 import DoneIcon from "@mui/icons-material/Done";
 import CloseIcon from "@mui/icons-material/Close";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import { useNavigate } from "react-router-dom";
 import { SchoolFormItem } from "../../../../../api/models/forms/schoolFormItem";
+import { Status } from "../../../../../api/models/forms/schoolFormStatus";
 
 interface FormListItemsProps {
   items: SchoolFormItem[];
@@ -18,24 +20,29 @@ interface FormListItemsProps {
 const FormListItems: React.FC<FormListItemsProps> = ({ items }) => {
   const navigate = useNavigate();
 
+  const renderStatus = (status: Status) => {
+    switch (status) {
+      case Status.None:
+        return <QuestionMarkIcon sx={{ color: "gray" }} />;
+      case Status.Approved:
+        return <DoneIcon sx={{ color: "green" }} />;
+      case Status.Refused:
+        return <CloseIcon sx={{ color: "red" }} />;
+    }
+  };
+
   return (
     <React.Fragment>
       {items.map((item, index) => {
         return (
           <>
-            <ListItem key={index}>
+            <ListItem key={item.id}>
               <ListItemButton
                 onClick={() =>
                   navigate(`/options/school/forms/editor/${item.id}`)
                 }
               >
-                <ListItemIcon>
-                  {item.approved ? (
-                    <DoneIcon sx={{ color: "green" }} />
-                  ) : (
-                    <CloseIcon sx={{ color: "red" }} />
-                  )}
-                </ListItemIcon>
+                <ListItemIcon>{renderStatus(item.status)}</ListItemIcon>
                 <ListItemText>{item.schoolName}</ListItemText>
               </ListItemButton>
             </ListItem>
